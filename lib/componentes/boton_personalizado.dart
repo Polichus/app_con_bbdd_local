07220ccdacd205693248ee1f07d0ba87d38a7301
Pol González Martínez <polgonzalez@ceroca.cat>
2025-02-05 16:54:39 +0100

    App acabada definitivamente

--- lib/componentes/boton_personalizado.dart
+++ lib/componentes/boton_personalizado.dart
@@ -3,23 +3,24 @@ import 'package:app_con_bbdd_local/componentes/pagina_registro.dart';
 import 'package:flutter/material.dart';
 
 class BotonPersonalizado extends StatelessWidget {
-
   final Function()? accioBoto;
 
-  const BotonPersonalizado({
-    super.key,
-    required this.accioBoto
-
-    
-  });
-
+  const BotonPersonalizado({super.key, required this.accioBoto});
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(
-        onPressed: accioBoto,
-        child: const Text("Confirmar Sesión"),
+      child: GestureDetector(
+        onTap: accioBoto,
+        child: Container(
+            decoration: BoxDecoration(
+              color: Colors.yellow[900],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text("Confirmar Sesión"),
+            )),
       ),
     );
   }
